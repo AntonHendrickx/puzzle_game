b3e2dac49c05abdb0b37f60d74d5c52f36b285df
AntonHendrickx <91948381+AntonHendrickx@users.noreply.github.com>
2024-05-30 23:48:09 +0200

    pieces stay within screen + options menu added

--- src/main.py
+++ src/main.py
@@ -16,6 +16,8 @@ class Game:
         self.puzzle = puzzle(50, 50, 600, 700, 12)
         self.start_button = button(350, 295, 100, 50, "Play", self.font, self.TEXT_COL, (42, 68, 81))
         self.resume_button = button(310, 500, 180, 50, "Resume", self.font, self.TEXT_COL, (42, 68, 81))
+        self.options_button = button(310, 400, 180, 50, "Options", self.font, self.TEXT_COL, (42, 68, 81))
+        self.back_button = button(340, 465, 120, 50, "Back", self.font, self.TEXT_COL, (42, 68, 81))
         self.exit_button = button(340, 500, 100, 50, "Exit", self.font, self.TEXT_COL, (42, 68, 81))
         self.quit_button = button(350, 355, 100, 50, "Quit", self.font, self.TEXT_COL, (42, 68, 81))
 
@@ -28,6 +30,8 @@ class Game:
         self.resume_button.change_position(self.screen.get_width() / 2 - 90, self.screen.get_height() / 2 - 5)
         self.exit_button.change_position(self.screen.get_width() / 2 - 60, self.screen.get_height() - 100)
         self.quit_button.change_position(self.screen.get_width() / 2 - 50, self.screen.get_height() / 2 + 55)
+        self.back_button.change_position(self.screen.get_width() / 2 - 60, self.screen.get_height() * 3 / 4 + 15)
+        self.options_button.change_position(self.screen.get_width() / 2 - 90, self.screen.get_height() * 2 / 3)
 
     def run_game(self):
         run = True
@@ -54,12 +58,18 @@ class Game:
                         self.game_state = state.PLAY
                     if self.exit_button.draw(self.screen):
                         self.game_state = state.MENU
+                    if self.options_button.draw(self.screen):
+                        self.game_state = state.OPTIONS
                 case state.MENU:
                     self.display_text("Puzzle", self.screen.get_width() / 2 - 75, self.screen.get_height() / 6)
                     if self.start_button.draw(self.screen):
                         self.game_state = state.PLAY
                     if self.quit_button.draw(self.screen):
                         run = False
+                case state.OPTIONS:
+                    self.display_text("Options", self.screen.get_width() / 2 - 90, self.screen.get_height() / 6)
+                    if self.back_button.draw(self.screen):
+                        self.game_state = state.PAUSED
                 case state.PLAY:
                     self.puzzle.draw(self.screen)
             self.resize()
--- src/regular_puzzle_piece.py
+++ src/regular_puzzle_piece.py
@@ -8,7 +8,15 @@ class regular_piece(piece):
         self.piece = pygame.Rect(x, y, size_x, size_y)
 
     def draw(self, surface):
-        super().draw(surface)
+        surface_width, surface_height = surface.get_size()
+        if self.piece.left < 0:
+            self.piece.left = 0
+        if self.piece.right > surface_width:
+            self.piece.right = surface_width
+        if self.piece.top < 0:
+            self.piece.top = 0
+        if self.piece.bottom > surface_height:
+            self.piece.bottom = surface_height
         pygame.draw.rect(surface, (255,255,255), self.piece)
 
     def check_collision(self, piece_tocheck):
