31e0eb28aceaae34428b4512e702efff7a8a15d8
AntonHendrickx <91948381+AntonHendrickx@users.noreply.github.com>
2024-06-04 00:38:52 +0200

    fixed testing + linter

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='puzzle_game',
     packages=['src'],
-    package_dir={'':'src'}
+    package_dir={'': 'src'}
 )
--- test/puzzle_test.py
+++ test/puzzle_test.py
@@ -8,20 +8,21 @@ from src.regular_puzzle_piece import RegularPiece
 
 class MyTestCase(unittest.TestCase):
     def test_save_piece(self):
-        image = pygame.image.load("../src/images/puzzle_test.png")
-        image = pygame.transform.scale(image, (50,50))
+        image = pygame.image.load("./test/puzzle_test.png")
+        image = pygame.transform.scale(image, (50, 50))
         piece = RegularPiece(1, 1, 50, 50, image)
         data = piece.serialize()
         new_piece = RegularPiece.deserialize(data)
         self.assertEqual(new_piece.get_width(), piece.get_width())  # add assertion here
 
     def test_save_puzzle(self):
-        image_path  = "../src/images/puzzle_test.png"
-        screen = pygame.Surface((800,600))
+        image_path = "./test/puzzle_test.png"
+        screen = pygame.Surface((800, 600))
         puzzle = Puzzle(screen, 600, 300, 8, image_path, False)
         data = puzzle.serialize()
         new_puzzle = Puzzle.deserialize(data, screen)
         self.assertIsNotNone(new_puzzle)
+
 
 if __name__ == '__main__':
     unittest.main()
